70b9a8f931ace3259ad63e8ad53defac3fb9ee79
yamatatsu <the.kinnikuman@gmail.com>
2017-01-04 16:38:51 +0900

    package update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
-import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import reducer from 'reducer.js'
-import Site from 'Container.js'
+import reducer from 'reducer'
+import Site from 'Container'
 
 const store = createStore(reducer)
 
