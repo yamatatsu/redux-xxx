1bb577f9f67e9d629b13e4381f9144d2e995d55f
yamatatsu <the.kinnikuman@gmail.com>
2017-01-04 17:03:58 +0900

    use thunk

--- src/Container.js
+++ src/Container.js
@@ -1,60 +1,17 @@
 // @flow
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import axios from 'axios'
-import _ from 'lodash'
 
 import Main from 'Component.jsx'
+import action from 'action.js'
 
-import type { Action } from 'action.js'
 import type { State } from 'reducer.js'
-
-const BASE_SHEET_ID = '1WsADSxHkAkWU5P8qYXm5BnaeVxZdo5XBhEcGJGiuDFY'
 
 const mapStateToProps = (state: State) => ({
   fetching: state.fetching,
   tables: state.tables,
 })
 
-const mapDispatchToProps = (dispatch: (action: Action) => void) => ({
-  handleClick: () => {
-    dispatch({ type: 'fetching.set', fetching: true })
-
-    getIds().then(ids => {
-      const promises = ids.map(id => (
-        getTable(id).then(table => {
-          dispatch({ type: 'tables.add', table })
-        })
-      ))
-
-      Promise.all(promises).then(() => {
-        dispatch({ type: 'fetching.set', fetching: false })
-      })
-    })
-  },
-})
+const mapDispatchToProps = (dispatch) => bindActionCreators(action, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main)
-
-// ================================
-// 部品
-
-function getIds () {
-  return getSheet(BASE_SHEET_ID)
-    .then(cells => cells.map(cell => cell.$t))
-}
-
-function getTable (id) {
-  return getSheet(id)
-    .then(cells => (
-      _(cells)
-        .groupBy(cell => cell.row - 1)
-        .map(row => _(row).map(cell => cell.$t).value())
-        .value()
-    ))
-}
-
-function getSheet (sheetId) {
-  return axios
-    .get(`https://spreadsheets.google.com/feeds/cells/${sheetId}/od6/public/values?alt=json`)
-    .then((response) => response.data.feed.entry.map(e => e.gs$cell))
-}
--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import reducer from 'reducer'
 import Site from 'Container'
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 render(
   <Provider store={store}>
